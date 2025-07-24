ed04f216bc5abf507ed0798717d660c3824dc254
vishaltoora <60240870+vishaltoora@users.noreply.github.com>
2025-07-24 12:10:19 -0700

    Update init_db.php

--- database/init_db.php
+++ database/init_db.php
@@ -1,6 +1,6 @@
 <?php
 // Initialize the SQLite database
-$db_path = __DIR__ . '/gt_automotives';
+$db_path = __DIR__ . '/gt_automotives.db';
 $schema_path = __DIR__ . '/schema.sql';
 
 // Create database connection
